df67a924f3726973f1bfe52e3edbf2c253d8006f
Eirik <eiriknd@stud.ntnu.no>
2022-05-20 12:42:50 +0200

    product cards restructured

--- index/src/products/ProductSection.js
+++ index/src/products/ProductSection.js
@@ -7,18 +7,26 @@ export default function ProductSection() {
         <div className="offerRow">
             <ProductCard img={require("../pictures/besoeksvenn.jpg")}
                          alt="Hund som ser glad ut"
-                         title="Besøksvenn!"
-                         text="Få besøk av et av våre mange kjærleike dyr"
+                         title="To dagers kurs!"
+                         text="Perfekt for nye medlemmer som ikke har tidligere kursing"
+                         duration="4 timer x 2"
+                         price="Pris: 8000 NOK"
+
             />
             <ProductCard img={require("../pictures/flyktningsvenn.jpg")}
                          alt="Mann som snakker med annen mann"
-                         title="Flyktningsvenn!"
-                         text="Snakk med noen som har annen kulturell bakgrunn"
+                         title="Dags kurs!"
+                         text="For de som trenger en oppfrisking i kunnskap eller har hatt kursing med oss de siste tre årene"
+                         duration="6 timer"
+                         price="Pris: 5000 NOK"
+
             />
             <ProductCard img={require("../pictures/rkungdom.jpg")}
                          alt="Glad ungdom i røde kors"
-                         title="Rødekors ungdom!"
-                         text="Bli medlem i røde kors ungdom"
+                         title="Kort konsultasjon!"
+                         text="Har du noe spesielt å ta opp med eller ønsker du bare noen å snakke med?"
+                         duration="60 minutter"
+                         price="Pris: 1600 NOK"
             />
         </div>
     )
