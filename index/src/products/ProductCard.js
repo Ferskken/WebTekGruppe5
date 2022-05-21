83c77d1320d900eb1aedbd7a9930d3c11b185a7b
Jones <jonesma@ntnu.no>
2022-05-21 22:02:52 +0200

    ProductCard - broken/bugged
    Some 'progress' on productcard, trying to make a new state save all the prop information of an old state, the select markup resets on change becuase of the value attribute, and form is being added separately.

--- index/src/products/ProductCard.js
+++ index/src/products/ProductCard.js
@@ -7,18 +7,27 @@ export default function ProductCard(props) {
 const cartContext = useContext(CartContext)
 const cartItems = cartContext.cartItems
 
-const [cartItemData, setCartItemData] = useState([])
+const [cartItemData, setCartItemData] = useState([props.product])
 
-    function handleChange(event) {
+function test() {
+        console.log(cartItemData)
+    }
+
+    function handleChangeCartItemChange(event) {
         console.log(event)
-        const {name, value, type, checked} = event.target
-        setCartItemData(prevFormData => {
+        const {name, value} = event.target
+
+        setCartItemData(prevCartItemData => {
             return {
-                ...prevFormData,
-                [name]: type === "checkbox" ? checked : value
+                ...prevCartItemData,
+                [name]: value
             }
         })
     }
+
+   function handleSubmitToCart(){
+
+   }
 
     return (
         <article className="articles">
@@ -34,7 +43,12 @@ const [cartItemData, setCartItemData] = useState([])
                         <br/>
                         <p>{"Vennligst kryss av under:"}</p>
                         <form>
-                            <select className="productCardDrop" name="TimeStampDrop">
+                            <select id="timeOfDay"
+                                    className="productCardDrop"
+                                    name="timeOfDay"
+                                    value={props.product.timeOfDay}
+                                    onChange={handleChangeCartItemChange}
+                            >
                                 <option value="hide">{"Tid på dagen"}</option>
                                 <option value="morning">{"10:00 - 14:00"}</option>
                                 <option value="evening">{"17:00 - 21:00"}</option>
@@ -42,7 +56,11 @@ const [cartItemData, setCartItemData] = useState([])
                             <br />
                             <br />
 
-                            <select className="productCardDrop" name="languageDrop">
+                            <select className="productCardDrop"
+                                    name="language"
+                                    value={props.product.language}
+                                    onChange={handleChangeCartItemChange}
+                            >
                                 <option value="hide">{"Språk"}</option>
                                 <option value="norwegian">{"Norsk"}</option>
                                 <option value="english">{"English"}</option>
@@ -50,7 +68,11 @@ const [cartItemData, setCartItemData] = useState([])
                             <br />
                             <br />
 
-                            <select className="productCardDrop" name="ParticipantDrop">
+                            <select className="productCardDrop"
+                                    name="groupSize"
+                                    value={props.product.groupSize}
+                                    onChange={handleChangeCartItemChange}
+                            >
                                 <option value="hide">{"Antall deltagere"}</option>
                                 <option value="1">{"1 (Alene)"}</option>
                                 <option value="5">{"5 (Gruppe)"}</option>
@@ -58,7 +80,11 @@ const [cartItemData, setCartItemData] = useState([])
                             <br />
                             <br />
 
-                            <select className="productCardDrop" name="dateDrop">
+                            <select className="productCardDrop"
+                                    name="date"
+                                    value={props.product.data}
+                                    onChange={handleChangeCartItemChange}
+                            >
                                 <option value="hide">{"Velg dato"}</option>
                                 <option value="dato1">{"1.januar"}</option>
                                 <option value="dato2">{"2.januar"}</option>
@@ -72,7 +98,7 @@ const [cartItemData, setCartItemData] = useState([])
                         <br/>
                     </div>
 
-                <button className="articleBtn" id="buyBtn"> Legg til kurs </button>
+                <button className="articleBtn" id="buyBtn" onClick={test}> Legg til kurs </button>
 
                 </div>
             </div>
