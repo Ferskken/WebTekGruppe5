221d8cf12888589ddaa3f35dad64b61a17deebfc
Eirik <eiriknd@stud.ntnu.no>
2022-05-21 02:20:37 +0200

    Reviews/Commentsection - CSS
    Constructed a new commentsection using dummy profiles currently. Also cleaned up in stylesheet discarding some unused code.

--- index/src/pages/LandingPage.js
+++ index/src/pages/LandingPage.js
@@ -36,6 +36,7 @@ export default class LandingPage extends React.Component {
                   </p>
               </div>
 
+              <Review />
 
           </main>
       </>
--- index/src/reviews/Review.js
+++ index/src/reviews/Review.js
@@ -2,34 +2,69 @@ import React from "react";
 
 export default function Review() {
     return(
-        <div className="card">
-            <div className="row">
-                <h4>Legg til en kommentar:</h4>
-                <div className="rating">
-                    <input type="radio" name="rating" value="5" id="5" />
-                    <label className="stars" htmlFor="5">☆</label>
-                    <input type="radio" name="rating" value="4" id="4" />
-                    <label className="stars" htmlFor="4">☆</label>
-                    <input type="radio" name="rating" value="3" id="3" />
-                    <label className="stars" htmlFor="3">☆</label>
-                    <input type="radio" name="rating" value="2" id="2" />
-                    <label className="stars" htmlFor="2">☆</label>
-                    <input type="radio" name="rating" value="1" id="1" />
-                    <label className="stars" htmlFor="1">☆</label>
-                </div>
-                <div className="comment-area">
-                            <textarea className="form-control" placeholder="what is your view?" rows="4">
-                            </textarea>
-                </div>
+                      <div id={"reviewBox"}>
 
-                <div className="commentbtn">
-                    <button className="cancelbtn">Avbryt</button>
-                </div>
+                          <div className="addNote"><input type="text" name="text" placeholder="+ Legg til kommentar" className="addTxt"/></div>
 
-                <div className="commentbtn">
-                    <button className="sendbtn">Send</button>
-                </div>
-            </div>
-        </div>
+                            <div className="commentBox">
+
+                                <div className="commentText">
+                                    <span className="text1">Red Cross are very competent experts in first help. They are so effective; you never
+                                                            need the second help.</span>
+
+                                    <div className="commentInfo">
+
+                                        <div className="commentProfile">
+                                          <img className="profilePicture" src="https://picsum.photos/200/300?random=1" alt="randomProfilePic"/>
+                                          <span className="text2">Billy Eirish</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div className="commentBox">
+
+                                <div className="commentText">
+                                    <span className="text1">I have been HR responsible at my company for 12 years, and every year I update
+                                                            my certification with Red Cross Ålesund. I can proudly say that no one has died under my
+                                                            watch.</span>
+
+                                    <div className="commentInfo">
+
+                                        <div className="commentProfile">
+                                           <img className="profilePicture" src="https://picsum.photos/200/300?random=2" alt="randomProfilePic"/>
+                                           <span className="text2">James Brown</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div className="commentBox">
+
+                                <div className="commentText">
+                                    <span className="text1">These folks may not be Linux kernel gurus, but they do one thing well – they teach
+                                                            you the essentials of first help in an easy and interesting way.</span>
+
+                                    <div className="commentInfo">
+
+                                        <div className="commentProfile">
+                                           <img className="profilePicture" src="https://picsum.photos/200/300?random=3" alt="randomProfilePic"/>
+                                           <span className="text2">Anna Brook</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+                      </div>
     )
 }
