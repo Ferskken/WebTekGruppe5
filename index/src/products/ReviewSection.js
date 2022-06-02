5021a44e40af09286693bda0530d106809d5090d
Jones <jonesma@ntnu.no>
2022-06-03 01:18:01 +0200

    Added useEffect to ReviewSection

--- index/src/products/ReviewSection.js
+++ index/src/products/ReviewSection.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import ProductReview from "./ProductReview";
 import {getCookie} from "../api/cookies";
