aea36091fa6191ba9f01490e99a9fe61ea7958c7
Eirik <eiriknd@stud.ntnu.no>
2022-06-02 19:16:05 +0200

    login info display user/admin page

--- index/src/displayyourpage/AdminPage.js
+++ index/src/displayyourpage/AdminPage.js
@@ -1,16 +1,31 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {unsetUser} from "../redux/userSlice";
+import {getAuthenticatedUser} from "../api/authentication";
+import {getCookie} from "../api/cookies";
+
+
+
 
 export default function AdminPage(props) {
 
+ function displayUser() {
+        if(getAuthenticatedUser()) {
+            return `Hei! ${getCookie("current_username")}`
+        } else {
+            return "Din side"
+        }
+    }
 
 
     return(
         <>
-            <div>
-                <button className={"logoutButton"} onClick={props.doLogout}>Logg ut Admin</button>
+            <div className={"logInInfo"}>
+            <h3 className={"logInName"}>{displayUser()}</h3>
+            <p className={"logInText"}>logget inn som Admin</p>
+                <button className={"logoutButton"} onClick={props.doLogout}>Logg ut</button>
             </div>
+
         </>
     )
 }
--- index/src/displayyourpage/UserPage.js
+++ index/src/displayyourpage/UserPage.js
@@ -1,10 +1,26 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {unsetUser} from "../redux/userSlice";
+import {getAuthenticatedUser} from "../api/authentication";
+import {getCookie} from "../api/cookies";
 
 export default function UserPage(props) {
+
+function displayUser() {
+        if(getAuthenticatedUser()) {
+            return `Hei! ${getCookie("current_username")}`
+        } else {
+            return "Din side"
+        }
+    }
+
     return(
         <>
-            <h1>User page</h1>
-            <button className={"logoutButton"} onClick={props.doLogout}>Logg ut Bruker</button>
+              <div className={"logInInfo"}>
+                        <h3 className={"logInName"}>{displayUser()}</h3>
+                        <p className={"logInText"}>logget inn som Bruker</p>
+                            <button className={"logoutButton"} onClick={props.doLogout}>Logg ut</button>
+                        </div>
         </>
     )
 }
